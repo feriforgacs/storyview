3e6a46fc689796c802c6bb4b825609629108c83b
Ferenc Forgacs <forgacsf@gmail.com>
2019-12-03 11:43:33 +0100

    Countdown

---
+++ _landing/assets/scripts/countdown.js
@@ -0,0 +1,29 @@
+const dayCounter = document.querySelector("#day__counter");
+const hourCounter = document.querySelector("#hour__counter");
+const minCounter = document.querySelector("#min__counter");
+const secCounter = document.querySelector("#sec__counter");
+const dealinfo = document.querySelector(".deal-info");
+const endDate = new Date("2019-12-07");
+
+function countdownTimer(){
+  const now = new Date();
+  const remainingTime = new Date(endDate.getTime() - now.getTime());
+
+  let remainingDays = remainingTime.getUTCDate() - 1;
+  let remainingHours = remainingTime.getUTCHours();
+  let remainingMinutes = remainingTime.getUTCMinutes();
+  let remainingSeconds = remainingTime.getUTCSeconds();
+
+  dayCounter.textContent = addZero(remainingDays);
+  hourCounter.textContent = addZero(remainingHours);
+  minCounter.textContent = addZero(remainingMinutes);
+  secCounter.textContent = addZero(remainingSeconds);
+}
+
+function addZero(value){
+  return value < 10 ? `0${value}` : value;
+}
+
+setInterval(countdownTimer, 1000);
+
+dealinfo.textContent = "Save $10 (28%) with the 🎅 Santa Deal until December 06.";
