6e81fd4766cdb3621b6c664ae8941ef1c133c894
Ferenc Forgacs <forgacsf@gmail.com>
2020-07-15 08:55:06 +0200

    Countdown update

--- _landing/assets/scripts/countdown.js
+++ _landing/assets/scripts/countdown.js
@@ -3,27 +3,27 @@ const hourCounter = document.querySelector("#hour__counter");
 const minCounter = document.querySelector("#min__counter");
 const secCounter = document.querySelector("#sec__counter");
 const dealinfo = document.querySelector(".deal-info");
-const endDate = new Date("July 14, 2020 23:59:59");
+const endDate = new Date("July 31, 2020 23:59:59");
 
-function countdownTimer(){
-  const now = new Date();
-  const remainingTime = Math.abs(endDate - now) / 1000;
-  
-  let remainingSeconds = Math.floor(remainingTime  % 60);
-  let remainingMinutes = Math.floor(remainingTime / 60) % 60;
-  let remainingHours = Math.floor(remainingTime / 3600) % 24;
-  let remainingDays = Math.floor(remainingTime / 86400);
-  
-  secCounter.textContent = addZero(remainingSeconds % 60);
-  minCounter.textContent = addZero(remainingMinutes % 60);
-  hourCounter.textContent = addZero(remainingHours);
-  dayCounter.textContent = addZero(remainingDays);
+function countdownTimer() {
+	const now = new Date();
+	const remainingTime = Math.abs(endDate - now) / 1000;
+
+	let remainingSeconds = Math.floor(remainingTime % 60);
+	let remainingMinutes = Math.floor(remainingTime / 60) % 60;
+	let remainingHours = Math.floor(remainingTime / 3600) % 24;
+	let remainingDays = Math.floor(remainingTime / 86400);
+
+	secCounter.textContent = addZero(remainingSeconds % 60);
+	minCounter.textContent = addZero(remainingMinutes % 60);
+	hourCounter.textContent = addZero(remainingHours);
+	dayCounter.textContent = addZero(remainingDays);
 }
 
-function addZero(value){
-  return value < 10 ? `0${value}` : value;
+function addZero(value) {
+	return value < 10 ? `0${value}` : value;
 }
 
 setInterval(countdownTimer, 1000);
 
-dealinfo.textContent = "🎉 Purchase the plugin before 14th of July and save $10 (30%) ✨";
+dealinfo.textContent = "🎉 Purchase the plugin before 31st of July and Save $10 (30%) ✨";
